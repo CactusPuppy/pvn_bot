7ec759fc9bd0584e98af2de4efc3bad10230c031
CactusPuppy <coby.zhong@gmail.com>
2018-07-12 21:13:48 -0700

    Initial commit

---
+++ src/main/java/usa/cactuspuppy/pvn_bot/JDAHandler.java
@@ -0,0 +1,32 @@
+package usa.cactuspuppy.pvn_bot;
+
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+import java.awt.*;
+
+public class JDAHandler extends ListenerAdapter {
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        if (event.getAuthor().isBot()) { return; }
+        Message message = event.getMessage();
+        String content = message.getContentRaw();
+        MessageChannel channel = event.getChannel();
+        if (content.startsWith("!poke ")) {
+            channel.sendMessage("Stop it I'm sleeping").queue();
+        } else if (content.equals("!ping ")) {
+            channel.sendMessage("Pong!").queue();
+        } else if (content.startsWith("!echo ")) {
+            EmbedBuilder builder = new EmbedBuilder();
+            builder.setTitle("Echoing " + event.getAuthor().getName() + "'s Message:");
+            builder.setColor(new Color(0xF40C0C));
+            content = content.substring("!echo ".length());
+            builder.addField("Message:", content, false);
+            channel.sendMessage(builder.build()).queue();
+        }
+    }
+}
---
+++ src/main/java/usa/cactuspuppy/pvn_bot/Main.java
@@ -0,0 +1,61 @@
+package usa.cactuspuppy.pvn_bot;
+
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import javax.security.auth.login.LoginException;
+import java.io.File;
+
+public class Main extends JavaPlugin {
+    private static Main instance;
+    private JDA jda;
+
+    @Override
+    public void onEnable() {
+        createConfig();
+        initDiscord();
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    private void createConfig() {
+        if (!getDataFolder().exists()) {
+            getLogger().info("Data folder not found, creating now...");
+            if (!getDataFolder().mkdirs()) {
+                getLogger().severe("Could not create data folder!");
+            }
+        }
+        File config = new File(getDataFolder(), "config.yml");
+        if (config.exists()) {
+            getLogger().info("Loading config.yml...");
+        } else {
+            getLogger().info("config.yml not found, creating now...");
+            saveDefaultConfig();
+        }
+    }
+
+    private void initDiscord() {
+        JDABuilder jdaBuilder = new JDABuilder(AccountType.BOT);
+        jdaBuilder.setToken(getConfig().getString("bot_token"));
+        jdaBuilder.addEventListener(new JDAHandler());
+        try {
+            jda = jdaBuilder.buildAsync();
+        } catch (LoginException e) {
+            getLogger().severe("Unable to initiate Discord integration!");
+            e.printStackTrace();
+        }
+    }
+
+    public static Main getInstance() {
+        return instance;
+    }
+
+    public JDA getJda() {
+        return jda;
+    }
+}
